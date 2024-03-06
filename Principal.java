88116b38109df8c18237e5de1bcedc2852963aaf
Madtore <143751703+Madtore@users.noreply.github.com>
2024-03-06 18:20:11 +0100

    Tercer commit

--- Principal.java
+++ Principal.java
@@ -3,6 +3,8 @@ package examenEdd.edd;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola a tod@s");
+		System.out.println("Examen de ED Trimestre 2");
+
 	}
 
 }
